d33bad3c31bff0939040a243cdd92ee62d688e6c
Roy Tan <roy@tenten.co>
2019-01-29 11:26:33 +0800

    [PHP] array sorting

---
+++ php/string/arraySorting.php
@@ -0,0 +1,95 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| SAMPLE
+|--------------------------------------------------------------------------
+|  SAMPLE Array
+|0 => 
+    array (size=8)
+      'ID' => string '3354' (length=4)
+      'post_name' => string 'sds-sis-4111a-4113a-4114a-4211a-4213a-4215a-4411a-vector-africa-en' (length=66)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2017/04/VECTORT-4111A-4113A-4114A-4211A-4213A-4215A-and-4411A-MSDS_AFRICA-ENGLISH.pdf' (length=121)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'AFRICA - EN' (length=11)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111A 4113A 4114A 4211A 4213A 4215A 4411A VECTOR' (length=48)
+  1 => 
+    array (size=8)
+      'ID' => string '3291' (length=4)
+      'post_name' => string 'sds-sis-4111n-4113n-4114n-4211n-4213n-vector-africa-en' (length=54)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2016/09/VECTOR-4111N-4113N-4114N-4116N-4211N-and-4213N-Styrenic-Block-Copolymers-SDS_AFRICA-EU-English.pdf' (length=142)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'AFRICA - EN' (length=11)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111N 4113N 4114N 4211N 4213N VECTOR' (length=36)
+  2 => 
+    array (size=8)
+      'ID' => string '12089' (length=5)
+      'post_name' => string 'sds-sis-4111a-4113a-4114a-4211a-4215a-4411a-vector-argentina-es' (length=63)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2017/11/VECTOR-4111A-4113A-4114A-4211A-4215A-and-4411A-Styrenic-Block-Copolymers-SDS_ARGENTINA-Spanish.pdf' (length=142)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'ARGENTINA - ES' (length=14)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111A 4113A 4114A 4211A 4215A 4411A VECTOR' (length=42)
+  3 => 
+    array (size=8)
+      'ID' => string '13076' (length=5)
+      'post_name' => string 'sds-sis-4111n-4113n-4114n-4116n-4211n-4213n-vector-argentina-es' (length=63)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2018/10/VECTOR-4111N-4113N-4114N-4116N-4211N-and-4213N-Styrenic-Block-Copolymers-SDS_ARGENTINA-Mexican-Spanish.pdf' (length=150)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'ARGENTINA - ES' (length=14)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111N 4113N 4114N 4116N 4211N 4213N VECTOR' (length=42)
+  4 => 
+    array (size=8)
+      'ID' => string '12845' (length=5)
+      'post_name' => string 'sds-sis-4111nd-4113nd-4114nd-4116nd-4211nd-4213nd-vector-argentina-es' (length=69)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2018/07/VECTOR-4111ND-4113ND-4114ND-4116ND-4211ND-and-4213ND-Styrenic-Block-Copolymers-SDS_ARGENTINA-Mexican-Spanish.pdf' (length=156)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'ARGENTINA - ES' (length=14)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111ND 4113ND 4114ND 4116ND 4211ND 4213ND VECTOR' (length=48)
+  5 => 
+    array (size=8)
+      'ID' => string '3356' (length=4)
+      'post_name' => string 'sds-sis-4111a-4113a-4114a-4211a-4213a-4215a-4411a-vector-australia-en' (length=69)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2017/04/VECTORT-4111A-4113A-4114A-4211A-4213A-4215A-and-4411A-MSDS_AUSTRALIA-ENGLISH.pdf' (length=124)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'AUSTRALIA - EN' (length=14)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111A 4113A 4114A 4211A 4213A 4215A 4411A VECTOR' (length=48)
+  6 => 
+    array (size=8)
+      'ID' => string '3293' (length=4)
+      'post_name' => string 'sds-sis-4111n-4113n-4114n-4211n-4213n-vector-australia-en' (length=57)
+      'url' => string 'https://www.tsrc.com.tw/app/uploads/2016/09/VECTOR-4111N-4113N-4114N-4116N-4211N-and-4213N-Styrenic-Block-Copolymers-SDS_AUSTRALIA-EU-English.pdf' (length=145)
+      'doc_type' => string 'GHS' (length=3)
+      'sheet_type' => string 'SDS' (length=3)
+      'language' => string 'AUSTRALIA - EN' (length=14)
+      'post_type' => string 'sis' (length=3)
+      'title' => string '4111N 4113N 4114N 4211N 4213N VECTOR' (length=36)
+
+| @param string $email 
+| @param int 42344 
+| @return string $code
+| @author roy <roy@tenten.co>
+| @version 1.0.0
+*/
+
+// multi sort
+    foreach ($search_arr as $key => $row)
+        {
+            $search_arr_value[$key] = $row['language'];
+            $search_arr_name[$key] = $row['title'];
+        }
+    array_multisort($search_arr_value, SORT_ASC, $search_arr_name, SORT_ASC, $search_arr);
+// single sort
+    usort($search_arr, function($a, $b) {
+        return $a['title'] <=> $b['title'];
+    });
